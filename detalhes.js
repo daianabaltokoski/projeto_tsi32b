a77b9b753f3ca0ca27106cd19408d7d011cb1c75
DaianaBalt <daianabalt@gmail.com>
2024-06-14 18:22:06 -0300

    inserir nome e sair em todas as páginas

--- detalhes.js
+++ detalhes.js
@@ -8,6 +8,7 @@ document.getElementById('search-form').addEventListener('submit', function (even
 });
 
 document.addEventListener("DOMContentLoaded", function() {
+    verificarLogin();
     // Endpoint da API para buscar os detalhes do livro
     const apiUrl = "https://gutendex.com/books/";
 
@@ -60,33 +61,3 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error("Erro na Busca:", error));
 });
-
-// Função para verificar se o usuário está logado
-function verificarLogin() {
-    const usuarioArmazenado = localStorage.getItem('usuario');
-    if (usuarioArmazenado) {
-        // Se o usuário estiver logado, exibir o nome e o botão "Sair"
-        const usuario = JSON.parse(usuarioArmazenado);
-        document.getElementById('nomeUsuario').textContent = usuario.nome;
-        document.getElementById('entreOuCadastrese').style.display = 'none';
-        document.getElementById('nomeUsuarioWrapper').style.display = 'block';
-    } else {
-        // Se o usuário não estiver logado, exibir o botão "Entrar ou Cadastre-se"
-        document.getElementById('entreOuCadastrese').style.display = 'block';
-        document.getElementById('nomeUsuarioWrapper').style.display = 'none';
-    }
-}
-
-// Função para sair da conta
-function sairDaConta() {
-    // Remover o usuário do localStorage
-    localStorage.removeItem('usuario');
-    // Atualizar a exibição
-    verificarLogin();
-}
-
-// Event listener para o botão "Sair"
-document.getElementById('logoutLink').addEventListener('click', sairDaConta);
-
-// Chamar a função de verificação de login ao carregar a página
-verificarLogin();
--- mais_lidos.js
+++ mais_lidos.js
@@ -51,34 +51,9 @@ async function carregarMaisBaixados() {
 window.addEventListener('load', () => {
     console.log('mais_lidos.js')
     carregarMaisBaixados();
+
+    verificarLogin();
+
 });
 
-// Função para verificar se o usuário está logado
-function verificarLogin() {
-    const usuarioArmazenado = localStorage.getItem('usuario');
-    if (usuarioArmazenado) {
-        // Se o usuário estiver logado, exibir o nome e o botão "Sair"
-        const usuario = JSON.parse(usuarioArmazenado);
-        document.getElementById('nomeUsuario').textContent = usuario.nome;
-        document.getElementById('entreOuCadastrese').style.display = 'none';
-        document.getElementById('nomeUsuarioWrapper').style.display = 'block';
-    } else {
-        // Se o usuário não estiver logado, exibir o botão "Entrar ou Cadastre-se"
-        document.getElementById('entreOuCadastrese').style.display = 'block';
-        document.getElementById('nomeUsuarioWrapper').style.display = 'none';
-    }
-}
 
-// // Função para sair da conta
-// function sairDaConta() {
-//     // Remover o usuário do localStorage
-//     localStorage.removeItem('usuario');
-//     // Atualizar a exibição
-//     verificarLogin();
-// }
-
-// Event listener para o botão "Sair"
-// document.getElementById('logoutLink').addEventListener('click', sairDaConta);
-
-// Chamar a função de verificação de login ao carregar a página
-verificarLogin();
